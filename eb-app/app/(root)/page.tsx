a7017535a5a224a9cfadc8bb6809448c6f286d52
Niorlys <niorlysernestoc@gmail.com>
2025-12-21 23:27:58 -0500

    Added header box component

--- eb-app/app/(root)/page.tsx
+++ eb-app/app/(root)/page.tsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
 
 const Home = () => {
+  const loggedInUser = {"firstName": "Niorlys", "lastName": "Mendez"};//await getLoggedInUser();
   return (
     <section className="home">
         <div className="home-content">
-            <header className="home-header">Wellcome back, Niorlys!!!</header>
+            <header className="home-header">
+                <HeaderBox
+                type="greeting"
+                title="Welcome back"
+                subtext="Access your account and manage your finances"
+                user={loggedInUser?.firstName || "Guest"}
+                 />
+            </header>
         </div>
     </section>
   )
