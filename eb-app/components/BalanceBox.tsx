ad5bcf0b5e5a7513c3578ce8d3ee15a1c322afaf
Niorlys <niorlysernestoc@gmail.com>
2025-12-24 23:47:08 -0500

    Added doughnut chart

--- eb-app/components/BalanceBox.tsx
+++ eb-app/components/BalanceBox.tsx
@@ -1,11 +1,13 @@
 
 import CountUpAnimation from '@/components/CountUpAnimation'
+import DoughnutChart from '@/components/DoughnutChart'
+
 
 const balance_box = ({accounts=[], totalBanks, totalCurrentBalance}: TotlaBalanceBoxProps) => {
   return (
     < section className="total-balance">
       <div className="total-balance-chart">
-        {/* Doughnut Chart */}
+        <DoughnutChart accounts={accounts} />
       </div>
       <div className="flex flex-col gap-6">
         <h2 className="header-2">
