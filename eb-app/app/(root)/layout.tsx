39e47b74c6b50576b1d98fed5350d2f32cabd112
Niorlys <niorlysernestoc@gmail.com>
2025-12-27 00:52:02 -0500

    Added side bar component

--- eb-app/app/(root)/layout.tsx
+++ eb-app/app/(root)/layout.tsx
@@ -1,11 +1,14 @@
+import SideBar from '@/components/SideBar'
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedInUser = {"firstName": "Niorlys", "lastName": "Mendez"};//await getLoggedInUser();
   return (
-    <main>
-        SIDEBAR
+    // Screen high + full width + font inter
+    <main className="flex h-screen w-full font-inter">
+        <SideBar user={loggedInUser as User} />
         {children}
     </main>
   );
---
+++ eb-app/components/SideBar.tsx
@@ -0,0 +1,42 @@
+'use client'
+import Link from 'next/link'
+import Image from 'next/image'
+import { sidebarLinks } from '@/constants'
+import { usePathname } from 'next/navigation'
+import { cn } from '@/lib/utils'
+
+const SideBar = ({user}: SiderbarProps) => {
+  const pathname = usePathname();
+  return (
+    <section className="side-bar">
+        <nav className="flex flex-col gap-4" >
+            <Link href="/" className="mb-12 cursor-pointer flex items-center gap-1">
+                <Image src="/icons/favicon.svg" alt="EasyBank" width={34} height={34}
+                className="size-[34px]
+                max-xl: size-18" />
+                <h1 className="sidebar-logo">EasyBank</h1>
+            </Link>
+            
+            {sidebarLinks.map((item) => {
+              const isActive = pathname === item.route 
+              || pathname.startsWith(`${item.route}/`);
+              return (
+                <Link href={item.route} key={item.label} className={cn('sidebar-link', {'bg-bank-gradient': isActive})}>
+                  <div className="relative size-6">
+                    <Image src={item.imgURL} alt={item.label} fill 
+                    className={cn({'brightness-[3] invert-0': isActive})} />  
+                  </div>
+                  <p className={cn('sidebar-label', {'!text-white': isActive})}>{item.label}</p>
+                </Link>
+              )
+            }      
+          )}
+
+
+        </nav>
+    
+    </section>
+  )
+}
+
+export default SideBar
