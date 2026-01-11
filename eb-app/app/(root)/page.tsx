97cc730492411e27555dfed816a1e8c590fc1917
Niorlys <niorlysernestoc@gmail.com>
2026-01-10 23:52:56 -0500

    Partial right side bar

--- eb-app/app/(root)/page.tsx
+++ eb-app/app/(root)/page.tsx
@@ -1,9 +1,21 @@
 import HeaderBox from '@/components/HeaderBox'
 import BalanceBox from '@/components/BalanceBox'
-
+import RightSideBar from '@/components/RightSideBar'
 
 const Home = () => {
-  const loggedInUser = {"firstName": "Niorlys", "lastName": "Mendez"};//await getLoggedInUser();
+  const loggedInUser = {"firstName": "Niorlys", "lastName": "Mendez", "email": "nioryls@gmail.com"};//await getLoggedInUser();
+  const sampleBanks = [
+    {
+      $id: '1',
+      name: 'Bank of America',
+      currentBalance: 100
+    },
+    {
+      $id: '2',
+      name: 'Crypto Bank',
+      currentBalance: 200
+    }
+  ]
   return (
     <section className="home">
         <div className="home-content">
@@ -20,7 +32,10 @@ const Home = () => {
                 totalBanks={1}
                 totalCurrentBalance={13405.56}
             />
+
+          RECENT TRANSACTIONS
         </div>
+        <RightSideBar user={loggedInUser as User} transactions={[]} banks={sampleBanks} />
     </section>
   )
 }
